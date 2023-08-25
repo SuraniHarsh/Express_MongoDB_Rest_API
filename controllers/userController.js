ae784af7c74c2c83edd2bef0046bf27105e94ad7
SuraniHarsh <harshsurani00@gmail.com>
2023-08-25 13:37:01 +0530

    controllers/userController.js : registerUser done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Get currentUser
 // @route GET /api/v1/users/current
@@ -12,7 +14,36 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 // @route POST /api/v1/users/register
 // @access public
 const registerUser = asyncHandler(async(req,res) => {
-    res.status(200).json({message : "Register User"});
+
+    try {
+
+    const {username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fildes are mandatory");
+    }
+
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        email,
+        username,
+        password: hashedPassword,
+    });
+
+    res.status(200).json({user : "User Registered Successfully", user : user})
+
+    } catch (error) {
+
+        res.status(500).json({message : "Error in User Register", Error : error.message});
+
+    }
+
 });
 
 
